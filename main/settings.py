a4ad99ea073093a892a81a66932be0cd62f12618
aemingenc <aemingenc@gmail.com>
2022-02-15 21:17:57 +0300

    base dır

--- main/settings.py
+++ main/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
